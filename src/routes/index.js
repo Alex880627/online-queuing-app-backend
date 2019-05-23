61a9dccbaf5bb4eacd1014786ab40173d96f8eb9
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-05-23 20:31:06 +0200

    Finished service details getter endpoint.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { userRegistration } = require ('../controllers/reg-user-controller')
-const { loginUser } = require ('../controllers/login-user-controller')
-const { loginProvider } = require('../controllers/login-provider-controller')
-const { providerRegistration }= require('../controllers/reg-provider-controller')
+const { userRegistration } = require ('../controllers/reg-user-controller');
+const { loginUser } = require ('../controllers/login-user-controller');
+const { loginProvider } = require('../controllers/login-provider-controller');
+const { providerRegistration } = require('../controllers/reg-provider-controller');
+const { getProvidersAndServicesByType } = require('../controllers/servicegetter-controller');
 
 router.post("/login-user", loginUser);
 router.post("/login-provider", loginProvider);
 router.post("/user-registration", userRegistration);
 router.post("/provider-registration", providerRegistration);
+router.get("/selected-type", getProvidersAndServicesByType);
 
 module.exports = router;
