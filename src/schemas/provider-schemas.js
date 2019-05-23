24b3b5ccad4698955c825c15d1def54d240be737
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2019-05-23 13:38:43 +0200

    Created schemas for project.

---
+++ src/schemas/provider-schemas.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const dailyScheduleSchema = new mongoose.Schema({ //  Provider should fill this up using mongoose add key (Schema#add) SHOULD BE BOOLEAN WITH FALSE
+  availableDay: {
+    type: Boolean,
+  },
+});
+
+const weeklyScheduleSchema = new mongoose.Schema({  //  dailySchema IDs come here
+  monday: mongoose.Schema.ObjectId,
+  tuesday: mongoose.Schema.ObjectId,
+  wednesday: mongoose.Schema.ObjectId,
+  thursday: mongoose.Schema.ObjectId,
+  friday: mongoose.Schema.ObjectId,
+});
+
+const serviceSchema = new mongoose.Schema({
+  serviceName: {
+    type: String,
+    maxlength: 40,
+  },
+  serviceDescription: {
+    type: String,
+    maxlength: 30,
+  },
+  weeklyScheduleId: {
+    type: mongoose.Schema.ObjectId,
+  }
+});
+
+const providerSchema = new mongoose.Schema({
+  providerType: {
+    type: String,
+    enum: ['hairdresser', 'dentist', 'massage therapist', 'plastic surgeon'],
+  },
+  companyname: { type: String, required: true, trim: true },
+  password: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true },
+  registerdate: { type: Date, default: Date.now },
+  services: [serviceSchema],
+});
+
+module.exports = {
+  dailyScheduleSchema,
+  weeklyScheduleSchema,
+  serviceSchema,
+  providerSchema,
+};
--- src/services/mail-services.js
+++ src/services/mail-services.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "illes.alexander88@gmail.com", //
+    user: process.env.GMAIL_ADRESS, //
     pass: process.env.GMAIL_PASS
   }
 });
